fdbe79b4e20a71fba95545ca0067d7e85e3ba8d2
Vo Anh Hoang <davidtheshadowvo13@gmail.com>
2024-10-08 01:19:59 +0700

    fix fe

--- src/pages/RegisterHeader/RegisterHeader.tsx
+++ src/pages/RegisterHeader/RegisterHeader.tsx
@@ -1,5 +1,7 @@
 import logo_indocs from '../../assets/logo_indocs.png'
 const RegisterHeader = () => {
+  const current = window.location.href
+  console.log(current)
   return (
     <header className="py-5 shadow">
       <div className="container bg-white  mx-auto flex justify-between items-center py-4 px-6">
@@ -10,7 +12,7 @@ const RegisterHeader = () => {
                 className=" w-32 h-auto" // Giữ nguyên các class CSS để đảm bảo kích thước phù hợp
               />
           <div className="ml-5 text-xl lg:text-2xl text-blue-500 ">
-            Đăng nhập
+            {current === "/login" ? "Đăng nhập": "Đăng ký"}
           </div>
         </nav>
         <div>
